abe581a1d295fcddf992a56aeb195f15bbff2f67
Iraxon <156947097+Iraxon@users.noreply.github.com>
2026-01-30 14:58:38 -0500

    Wrote EntityFromUUIDProcedure manually

--- src/main/java/com/github/iraxon/procedures/EntityFromUUIDProcedure.java
+++ src/main/java/com/github/iraxon/procedures/EntityFromUUIDProcedure.java
@@ -6,21 +6,29 @@ import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.entity.Entity;
 
 import java.util.Comparator;
+import java.util.Objects;
+
+import javax.annotation.Nullable;
 
 public class EntityFromUUIDProcedure {
-	public static Entity execute(LevelAccessor world, double x, double y, double z, String uuid) {
-		if (uuid == null)
-			return null;
-		Entity rVal = null;
-		{
-			final Vec3 _center = new Vec3(x, y, z);
-			for (Entity entityiterator : world.getEntitiesOfClass(Entity.class, new AABB(_center, _center).inflate(64 / 2d), e -> true).stream().sorted(Comparator.comparingDouble(_entcnd -> _entcnd.distanceToSqr(_center))).toList()) {
-				if ((entityiterator.getStringUUID()).equals(uuid)) {
-					rVal = entityiterator;
-					break;
-				}
-			}
-		}
-		return rVal;
+
+	/**
+	 * Range is a 64x64x64 box
+	 *
+	 * @param world
+	 * @param x
+	 * @param y
+	 * @param z
+	 * @param uuid
+	 * @return
+	 */
+	public static @Nullable Entity execute(LevelAccessor world, double x, double y, double z, String uuid) {
+		Objects.requireNonNull(world);
+		Objects.requireNonNull(uuid);
+
+		final Vec3 center = new Vec3(x, y, z);
+		return world.getEntitiesOfClass(Entity.class, new AABB(center, center).inflate(64 / 2d),
+				entity -> entity.getStringUUID().equals(uuid)).stream()
+				.min(Comparator.comparingDouble(entity -> entity.distanceToSqr(center))).orElse(null);
 	}
 }
