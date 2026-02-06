486beb73e29529cfe84fd02296ac591cfa7cce75
Iraxon <156947097+Iraxon@users.noreply.github.com>
2026-02-06 11:33:04 -0500

    Orders and Directions

--- src/main/java/com/github/iraxon/procedures/FormationStateNBTWrapper.java
+++ src/main/java/com/github/iraxon/procedures/FormationStateNBTWrapper.java
@@ -1,5 +1,7 @@
 package com.github.iraxon.procedures;
 
+import java.util.Objects;
+import java.util.Set;
 import java.util.stream.Stream;
 
 import javax.annotation.Nonnull;
@@ -14,34 +16,134 @@ public record FormationStateNBTWrapper(CompoundTag data) {
      *
      * @return
      */
-    public FormationStateNBTWrapper of(CompoundTag data) {
-        return new FormationStateNBTWrapper(data);
+    public FormationStateNBTWrapper of(@Nonnull CompoundTag data) {
+        return new FormationStateNBTWrapper(Objects.requireNonNull(data));
     }
 
-    public static final String FormationKey = "formation";
+    public static final String FORMATION_KEY = "formation";
 
     public enum Formation {
-        LINE(0),
-        SQUARE(1),
-        COLUMN(2);
+        LINE(0, Set.of(Order.HALT, Order.ADVANCE, Order.CHARGE), true),
+        SQUARE(1, Set.of(Order.HALT, Order.ADVANCE), true),
+        COLUMN(2, Set.of(Order.HALT, Order.FOLLOW, Order.COLLECT), false),
+        NARROW_COLUMN(2, Set.of(Order.HALT, Order.FOLLOW, Order.COLLECT), false);
 
         public final int index;
+        /**
+         * Do not mutate
+         */
+        public final Set<Order> validOrders;
 
-        private Formation(int index) {
+        public final boolean uses_direction;
+
+        private Formation(int index, Set<Order> validOrders, boolean uses_direction) {
             this.index = index;
+            this.validOrders = validOrders;
+            this.uses_direction = uses_direction;
         }
 
+        @SuppressWarnings("null")
+        @Nonnull
         public static Formation get(int index) {
             return Stream.of(values()).filter(v -> v.index == index).findAny().orElse(COLUMN);
         }
     }
 
-    public void setFormation(Formation f) {
-        data.putInt(FormationKey, f.index);
+    public void setFormation(@Nonnull Formation f) {
+        Objects.requireNonNull(f);
+        data.putInt(FORMATION_KEY, f.index);
     }
 
+    @Nonnull
     public Formation formation() {
-        return Formation.get(data.getInt(FormationKey));
+        return Formation.get(data.getInt(FORMATION_KEY));
+    }
+
+    public static final String ORDER_KEY = "order";
+
+    public enum Order {
+        HALT(0),
+        ADVANCE(1),
+        CHARGE(2),
+        FOLLOW(3),
+        COLLECT(4);
+
+        public final int index;
+
+        private Order(int index) {
+            this.index = index;
+        }
+
+        public boolean is_valid_for(Formation f) {
+            return f.validOrders.contains(this);
+        }
+
+        @SuppressWarnings("null")
+        @Nonnull
+        public static Order get(int index) {
+            return Stream.of(values()).filter(v -> v.index == index).findAny().orElse(HALT);
+        }
+    }
+
+    public void setOrder(@Nonnull Order o) {
+
+        Objects.requireNonNull(o);
+
+        if (o.is_valid_for(formation()))
+            data.putInt(ORDER_KEY, o.index);
+
+        else
+            data.putInt(ORDER_KEY, Order.HALT.index);
+    }
+
+    @Nonnull
+    public Order order() {
+        return Order.get(data.getInt(ORDER_KEY));
+    }
+
+    public static final String DIRECTION_KEY = "direction";
+
+    public enum Direction {
+        SOUTH(0),
+        SOUTHWEST(1),
+        WEST(2),
+        NORTHWEST(3),
+        NORTH(4),
+        NORTHEAST(5),
+        EAST(6),
+        SOUTHEAST(7);
+
+        public final int index;
+
+        private Direction(int index) {
+            this.index = index;
+        }
+
+        public boolean is_valid_for(Formation f) {
+            return f.uses_direction;
+        }
+
+        @SuppressWarnings("null")
+        @Nonnull
+        public static Direction get(int index) {
+            return Stream.of(values()).filter(v -> v.index == index).findAny().orElse(SOUTH);
+        }
+    }
+
+    public void setDirection(@Nonnull Direction d) {
+
+        Objects.requireNonNull(d);
+
+        if (d.is_valid_for(formation()))
+            data.putInt(DIRECTION_KEY, d.index);
+
+        else
+            data.putInt(DIRECTION_KEY, Direction.SOUTH.index);
+    }
+
+    @Nonnull
+    public Direction direction() {
+        return Direction.get(data.getInt(ORDER_KEY));
     }
 
 }
