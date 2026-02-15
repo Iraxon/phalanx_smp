27b9ed356e95e30cc8ff178dcf8b61f2916046ff
Iraxon <156947097+Iraxon@users.noreply.github.com>
2026-02-14 23:29:25 -0500

    Fixed imports

--- src/main/java/com/github/iraxon/procedures/IssueOrdersInput2OnKeyPressedProcedure.java
+++ src/main/java/com/github/iraxon/procedures/IssueOrdersInput2OnKeyPressedProcedure.java
@@ -2,9 +2,7 @@ package com.github.iraxon.procedures;
 
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.damagesource.DamageTypes;
-import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.core.registries.Registries;
+import com.github.iraxon.procedures.OrderHandling.CommandInput;
 
 public class IssueOrdersInput2OnKeyPressedProcedure {
 	public static void execute(LevelAccessor world, Entity entity) {
