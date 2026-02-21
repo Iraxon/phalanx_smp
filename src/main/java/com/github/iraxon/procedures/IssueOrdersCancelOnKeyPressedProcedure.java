dd9665c3563e324e7c9054fbd54e71111b123b73
Iraxon <156947097+Iraxon@users.noreply.github.com>
2026-02-20 21:22:18 -0500

    Implemented keypress procedures

--- src/main/java/com/github/iraxon/procedures/IssueOrdersCancelOnKeyPressedProcedure.java
+++ src/main/java/com/github/iraxon/procedures/IssueOrdersCancelOnKeyPressedProcedure.java
@@ -2,17 +2,10 @@ package com.github.iraxon.procedures;
 
 import net.minecraft.world.entity.Entity;
 
-import com.github.iraxon.network.PhalanxSmpModVariables;
-
 public class IssueOrdersCancelOnKeyPressedProcedure {
 	public static void execute(Entity entity) {
 		if (entity == null)
 			return;
-		{
-			entity.getCapability(PhalanxSmpModVariables.PLAYER_VARIABLES).ifPresent(capability -> {
-				capability.isGivingOrders = false;
-				capability.markSyncDirty();
-			});
-		}
+		OrderManager.of(entity).inputCancel();
 	}
 }
