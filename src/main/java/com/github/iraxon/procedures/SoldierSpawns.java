49230a4a83b0d251d0c9ab5a9ecb82ee691463d5
Iraxon <156947097+Iraxon@users.noreply.github.com>
2026-03-22 20:19:26 -0400

    Added SoldierSpawns

---
+++ src/main/java/com/github/iraxon/procedures/SoldierSpawns.java
@@ -0,0 +1,48 @@
+package com.github.iraxon.procedures;
+
+import java.util.Objects;
+import java.util.Optional;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import com.github.iraxon.entity.DeepslateGolemEntity;
+import com.github.iraxon.init.PhalanxSmpModEntities;
+import com.github.iraxon.procedures.SoldierNBT.SoldierType;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.MobSpawnType;
+
+public class SoldierSpawns {
+
+    public static Optional<DeepslateGolemEntity> spawn(@Nonnull ServerLevel world, @Nonnull BlockPos position) {
+        return spawn(world, position, SoldierType.COMMANDER, null);
+    }
+
+    public static Optional<DeepslateGolemEntity> spawn(@Nonnull ServerLevel world, @Nonnull BlockPos position,
+            @Nonnull SoldierType type,
+            @Nullable DeepslateGolemEntity commander) {
+
+        if (!type.equals(SoldierType.COMMANDER) && commander == null) {
+            Objects.requireNonNull(commander);
+        }
+
+        final var rVal = golem(world, position);
+        rVal.ifPresent(
+                (@Nonnull DeepslateGolemEntity golem) -> {
+                    @Nonnull
+                    final var true_commander = commander == null ? golem : commander;
+                    SoldierNBT.setType(golem, type);
+                    SoldierNBT.setCommander(golem, true_commander);
+                });
+        return rVal;
+    }
+
+    @Nonnull
+    private static Optional<DeepslateGolemEntity> golem(@Nonnull ServerLevel world, @Nonnull BlockPos position) {
+        return Objects.requireNonNull(Optional.ofNullable(
+                PhalanxSmpModEntities.DEEPSLATE_GOLEM.get().spawn(world, position, MobSpawnType.MOB_SUMMONED)));
+    }
+
+}
