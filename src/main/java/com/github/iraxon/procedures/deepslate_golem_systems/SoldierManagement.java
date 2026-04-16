ca20d66ca531ad8855c6c029c2fb96da293ba235
Iraxon <156947097+Iraxon@users.noreply.github.com>
2026-04-15 20:12:16 -0400

    SoldierManagement utility

---
+++ src/main/java/com/github/iraxon/procedures/deepslate_golem_systems/SoldierManagement.java
@@ -0,0 +1,129 @@
+package com.github.iraxon.procedures.deepslate_golem_systems;
+
+import java.util.Objects;
+
+import javax.annotation.Nullable;
+
+import com.github.iraxon.procedures.PhalanxUtils;
+import com.github.iraxon.procedures.PhalanxUtils.Vec3NBT;
+
+import net.minecraft.commands.arguments.EntityAnchorArgument;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.Mob;
+import net.minecraft.world.phys.Vec3;
+
+public class SoldierManagement {
+
+    private static final String KEY_STATE_TYPE = "phalanx_soldier_state_type";
+    private static final String KEY_STATE_PAYLOAD = "phalanx_soldier_state_payload";
+
+    private static final String STATE_TYPE_STRING_MOVE = "move";
+    private static final String STATE_TYPE_STRING_FOLLOW = "follow";
+    private static final String STATE_TYPE_STRING_HALT = "halt";
+
+    private static final String KEY_MOVE_STATE_MOVE_POSITION = "movePosition";
+    private static final String KEY_MOVE_STATE_LOOK_VECTOR = "lookVector";
+
+    private static final String KEY_FOLLOW_STATE_TARGET = "target";
+
+    public static void soldierMove(Mob soldier, @Nullable Vec3 moveTarget, @Nullable Vec3 lookVector) {
+        if (soldier == null) {
+            return;
+        }
+        final var data = soldier.getPersistentData();
+        data.putString(KEY_STATE_TYPE, STATE_TYPE_STRING_MOVE);
+        var payload = new CompoundTag();
+        Vec3NBT.storeVec3(payload, KEY_MOVE_STATE_MOVE_POSITION, moveTarget);
+        Vec3NBT.storeVec3(payload, KEY_MOVE_STATE_LOOK_VECTOR, lookVector);
+        data.put(KEY_STATE_PAYLOAD, payload);
+    }
+
+    public static void soldierFollow(Mob soldier, LivingEntity followTarget) {
+        if (soldier == null) {
+            return;
+        }
+        final var data = soldier.getPersistentData();
+        data.putString(KEY_STATE_TYPE, STATE_TYPE_STRING_FOLLOW);
+        var payload = new CompoundTag();
+        payload.putString(KEY_FOLLOW_STATE_TARGET, Objects.requireNonNull(followTarget.getStringUUID()));
+        data.put(KEY_STATE_PAYLOAD, payload);
+    }
+
+    public static void soldierHalt(Mob soldier) {
+        if (soldier == null) {
+            return;
+        }
+        final var data = soldier.getPersistentData();
+        data.putString(KEY_STATE_TYPE, STATE_TYPE_STRING_HALT);
+        data.remove(KEY_STATE_PAYLOAD);
+    }
+
+    public static void soldierTick(Mob soldier) {
+        if (soldier == null) {
+            return;
+        }
+        final var data = soldier.getPersistentData();
+        final var typeString = data.getString(KEY_STATE_TYPE);
+
+        switch (typeString) {
+            case STATE_TYPE_STRING_MOVE -> {
+                final var payload = Objects.requireNonNull(data.getCompound(KEY_STATE_PAYLOAD));
+                standAtTick(soldier, Vec3NBT.retrieveVec3(payload, KEY_MOVE_STATE_MOVE_POSITION),
+                        Vec3NBT.retrieveVec3(payload, KEY_MOVE_STATE_LOOK_VECTOR));
+            }
+            case STATE_TYPE_STRING_FOLLOW ->
+                PhalanxUtils.getEntityByUUID(
+                        Objects.requireNonNull(soldier.level()),
+                        LivingEntity.class,
+                        Objects.requireNonNull(soldier.position()), 64,
+                        data.getCompound(KEY_STATE_PAYLOAD).getString(KEY_FOLLOW_STATE_TARGET))
+                        .ifPresent(target -> followEntityTick(soldier, target));
+
+            // default is "halt"
+            default -> standAtTick(soldier, null, null);
+        }
+    }
+
+    private static void standAtTick(Mob soldier, @Nullable Vec3 movePosition, @Nullable Vec3 lookVector) {
+
+        if (soldier == null) {
+            return;
+        }
+
+        final boolean atMovePosition;
+
+        if (movePosition == null) {
+            soldier.getNavigation().stop();
+            atMovePosition = true;
+        } else {
+            atMovePosition = movePosition.subtract(Objects.requireNonNull(soldier.position()))
+                    .horizontalDistanceSqr() < 0.75 * 0.75;
+
+            if (!atMovePosition) {
+                soldier.getNavigation().moveTo(movePosition.x, movePosition.y, movePosition.z, 1.0);
+            }
+        }
+
+        if (atMovePosition && lookVector != null) {
+            soldier.lookAt(EntityAnchorArgument.Anchor.EYES,
+                    Objects.requireNonNull(soldier.getEyePosition().add(lookVector)));
+        }
+    }
+
+    private static void followEntityTick(Mob soldier, Entity target) {
+        if (soldier == null || target == null) {
+            return;
+        }
+        soldier.getNavigation().moveTo(target, 1.3);
+    }
+
+    private static void attackEntity(Mob soldier, LivingEntity target) {
+        if (soldier == null || target == null) {
+            return;
+        }
+        followEntityTick(soldier, target);
+        soldier.setTarget(target);
+    }
+}
