30d42bcb1b16e6f952022ac3e0ac7418ea4dc1b0
Iraxon <156947097+Iraxon@users.noreply.github.com>
2026-04-15 21:27:34 -0400

    Commented out for now: Soldier Spawns

--- src/main/java/com/github/iraxon/procedures/SoldierSpawns.java
+++ src/main/java/com/github/iraxon/procedures/SoldierSpawns.java
@@ -1,55 +1,55 @@
-package com.github.iraxon.procedures;
+// package com.github.iraxon.procedures;
 
-import java.util.Objects;
-import java.util.Optional;
+// import java.util.Objects;
+// import java.util.Optional;
 
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
+// import javax.annotation.Nonnull;
+// import javax.annotation.Nullable;
 
-import com.github.iraxon.entity.DeepslateGolemEntity;
-import com.github.iraxon.init.PhalanxSmpModEntities;
-import com.github.iraxon.procedures.SoldierNBT.SoldierType;
+// import com.github.iraxon.entity.DeepslateGolemEntity;
+// import com.github.iraxon.init.PhalanxSmpModEntities;
+// import com.github.iraxon.procedures.SoldierNBT.SoldierType;
 
-import net.minecraft.core.BlockPos;
-import net.minecraft.server.level.ServerLevel;
-import net.minecraft.world.entity.MobSpawnType;
+// import net.minecraft.core.BlockPos;
+// import net.minecraft.server.level.ServerLevel;
+// import net.minecraft.world.entity.MobSpawnType;
 
-public class SoldierSpawns {
+// public class SoldierSpawns {
 
-    @Nonnull
-    public static Optional<DeepslateGolemEntity> spawnCommander(@Nonnull ServerLevel world,
-            @Nonnull BlockPos position) {
-        return _spawn(world, position, SoldierType.COMMANDER, null);
-    }
+//     @Nonnull
+//     public static Optional<DeepslateGolemEntity> spawnCommander(@Nonnull ServerLevel world,
+//             @Nonnull BlockPos position) {
+//         return _spawn(world, position, SoldierType.COMMANDER, null);
+//     }
 
-    @Nonnull
-    public static Optional<DeepslateGolemEntity> spawn(@Nonnull ServerLevel world, @Nonnull BlockPos position,
-            @Nonnull SoldierType type,
-            @Nonnull DeepslateGolemEntity commander) {
-        return _spawn(world, position, type, commander);
-    }
+//     @Nonnull
+//     public static Optional<DeepslateGolemEntity> spawn(@Nonnull ServerLevel world, @Nonnull BlockPos position,
+//             @Nonnull SoldierType type,
+//             @Nonnull DeepslateGolemEntity commander) {
+//         return _spawn(world, position, type, commander);
+//     }
 
-    @Nonnull
-    private static Optional<DeepslateGolemEntity> _spawn(@Nonnull ServerLevel world, @Nonnull BlockPos position,
-            @Nonnull SoldierType type,
-            @Nullable DeepslateGolemEntity commander) {
+//     @Nonnull
+//     private static Optional<DeepslateGolemEntity> _spawn(@Nonnull ServerLevel world, @Nonnull BlockPos position,
+//             @Nonnull SoldierType type,
+//             @Nullable DeepslateGolemEntity commander) {
 
-        final var rVal = golem(world, position);
-        rVal.ifPresent(
-                (@Nonnull DeepslateGolemEntity golem) -> {
-                    @Nonnull
-                    final var true_commander = (commander == null || type.equals(SoldierType.COMMANDER)) ? golem
-                            : commander;
-                    SoldierNBT.setType(golem, type);
-                    SoldierNBT.setCommander(golem, true_commander);
-                });
-        return rVal;
-    }
+//         final var rVal = golem(world, position);
+//         rVal.ifPresent(
+//                 (@Nonnull DeepslateGolemEntity golem) -> {
+//                     @Nonnull
+//                     final var true_commander = (commander == null || type.equals(SoldierType.COMMANDER)) ? golem
+//                             : commander;
+//                     SoldierNBT.setType(golem, type);
+//                     SoldierNBT.setCommander(golem, true_commander);
+//                 });
+//         return rVal;
+//     }
 
-    @Nonnull
-    private static Optional<DeepslateGolemEntity> golem(@Nonnull ServerLevel world, @Nonnull BlockPos position) {
-        return Objects.requireNonNull(Optional.ofNullable(
-                PhalanxSmpModEntities.DEEPSLATE_GOLEM.get().spawn(world, position, MobSpawnType.MOB_SUMMONED)));
-    }
+//     @Nonnull
+//     private static Optional<DeepslateGolemEntity> golem(@Nonnull ServerLevel world, @Nonnull BlockPos position) {
+//         return Objects.requireNonNull(Optional.ofNullable(
+//                 PhalanxSmpModEntities.DEEPSLATE_GOLEM.get().spawn(world, position, MobSpawnType.MOB_SUMMONED)));
+//     }
 
-}
+// }
