298806a1f6228b6ba57c088dd7ddbeac65d34923
Iraxon <156947097+Iraxon@users.noreply.github.com>
2026-04-15 23:35:03 -0400

    SoldierType

---
+++ src/main/java/com/github/iraxon/procedures/deepslate_golem_systems/SoldierType.java
@@ -0,0 +1,23 @@
+package com.github.iraxon.procedures.deepslate_golem_systems;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+public enum SoldierType {
+        COMMANDER("commander"),
+        HEAVY_INFANTRY("heavy_infantry");
+
+        private final String name;
+
+        private SoldierType(String name) {
+            this.name = name;
+        }
+
+        public String encode() {
+            return this.name;
+        }
+
+        public static Optional<SoldierType> decode(String name) {
+            return Arrays.stream(values()).filter(t -> name.equals(t.name)).findAny();
+        }
+    }
