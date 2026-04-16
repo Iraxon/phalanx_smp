0d19d2a5e1b6619576a5cb5ab1d841135932e81f
Iraxon <156947097+Iraxon@users.noreply.github.com>
2026-04-15 21:29:47 -0400

    Commented out for now

--- src/main/java/com/github/iraxon/procedures/UpdateLastAttackerProcedure.java
+++ src/main/java/com/github/iraxon/procedures/UpdateLastAttackerProcedure.java
@@ -9,15 +9,16 @@ import com.github.iraxon.procedures.DeepslateGolemAIProcedure.SoldierAIUtils;
 
 public class UpdateLastAttackerProcedure {
 	public static boolean execute(LevelAccessor world, Entity entity, Entity sourceentity) {
-		if (!(entity instanceof DeepslateGolemEntity golem && sourceentity instanceof LivingEntity attacker)) {
-			return false;
-		}
-		if (SoldierAIUtils.isAllied(golem, attacker) && !(attacker instanceof Player)) {
-			return false;
-		} else {
-			SoldierNBT.setLastAttackerUUID(golem, attacker);
-			return true;
-		}
+		// if (!(entity instanceof DeepslateGolemEntity golem && sourceentity instanceof LivingEntity attacker)) {
+		// 	return false;
+		// }
+		// if (SoldierAIUtils.isAllied(golem, attacker) && !(attacker instanceof Player)) {
+		// 	return false;
+		// } else {
+		// 	SoldierNBT.setLastAttackerUUID(golem, attacker);
+		// 	return true;
+		// }
 
+		return true;
 	}
 }
